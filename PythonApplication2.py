7ee2fa52c67efb6a1c39b9e83ec21443b4bc0b85
mareaokim <39861011+mareaokim@users.noreply.github.com>
2018-06-11 03:35:00 +0900

    Add files via upload

---
+++ PythonApplication2.py
@@ -0,0 +1,14 @@
+#10년 이자 계산기(p.48)
+def main():
+    print("This program calculates the future value")
+    print("of a 10-year investment.")
+
+    principal = eval(input("Enter the initial principal : "))
+    apr = eval(input("Enter the annual interest rate: ")) 
+
+    for i in range(10):
+        principal = principal * (1+apr)
+        print("The value in 10 years is :" , principal)
+
+        
+main()
